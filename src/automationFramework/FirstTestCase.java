2fafac52ddd9bcbfd02877481a85fb3fa20bd075
unknown <muh.arizki@gmail.com>
2025-07-10 15:44:56 +0700

    uploaded

---
+++ src/automationFramework/FirstTestCase.java
@@ -0,0 +1,33 @@
+package automationFramework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTestCase {
+	public static void main(String[] args) {
+		// Set the system property for ChromeDriver (path to chromedriver executable)
+		System.setProperty("webdriver.chrome.driver", "C:\\ToolsQA\\chromedriver-win64\\chromedriver.exe");
+
+		// Create an instance of ChromeDriver (launch the Chrome browser)
+		WebDriver driver = new ChromeDriver();
+
+		try {
+			// Navigate to the desired website ()
+			driver.get("https://practicetestautomation.com");
+			// Get and print the page title
+			String pageTitle = driver.getTitle();
+			System.out.println("Page Title: " + pageTitle);
+
+			// Wait for a few seconds (for demonstration purposes only)
+			Thread.sleep(3000);
+			System.out.println("Execution complete");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			// Close the browser
+			driver.quit();
+		}
+	}
+}
---
+++ src/automationFramework/LoginTest.java
@@ -0,0 +1,41 @@
+package automationFramework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+    public static void main(String[] args) {
+        // Path ke chromedriver
+        System.setProperty("webdriver.chrome.driver", "C:\\ToolsQA\\chromedriver-win64\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("https://practicetestautomation.com/practice-test-login/");
+
+            // Isi username
+            WebElement username = driver.findElement(By.id("username"));
+            username.sendKeys("student");
+
+            // Isi password
+            WebElement password = driver.findElement(By.id("password"));
+            password.sendKeys("Password123");
+
+            // Klik login
+            WebElement loginButton = driver.findElement(By.id("submit"));
+            loginButton.click();
+
+            Thread.sleep(2000); // tunggu 2 detik
+
+            // Cek hasil login
+            String successText = driver.findElement(By.tagName("h1")).getText();
+            System.out.println("Login result: " + successText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit(); // tutup browser
+        }
+    }
+}
