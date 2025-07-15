733fae6c349ac4421f8109567d2aef5dc63f4a64
unknown <muh.arizki@gmail.com>
2025-07-15 13:29:08 +0700

    add logout after success login

--- src/automationFramework/LoginTest.java
+++ src/automationFramework/LoginTest.java
@@ -4,8 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 public class LoginTest {
+	// menunggu semua elemen di load
+	public static WebElement waitVisible(WebDriver driver, By locator) {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	    return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+	}
     public static void main(String[] args) {
         // Path ke chromedriver
         System.setProperty("webdriver.chrome.driver", "C:\\ToolsQA\\chromedriver-win64\\chromedriver.exe");
@@ -14,9 +22,9 @@ public class LoginTest {
 
         try {
             driver.get("https://practicetestautomation.com/practice-test-login/");
-
+            
             // Isi username
-            WebElement username = driver.findElement(By.id("username"));
+            WebElement username = waitVisible(driver,By.id("username"));
             username.sendKeys("student");
 
             // Isi password
@@ -27,16 +35,28 @@ public class LoginTest {
             WebElement loginButton = driver.findElement(By.id("submit"));
             loginButton.click();
 
-            // tunggu 2 detik
-            Thread.sleep(2000); 
 
             // Cek hasil login
-            String successText = driver.findElement(By.tagName("h1")).getText();
-            System.out.println("Login result: " + successText);
+            String successText = waitVisible(driver,By.tagName("h1")).getText();
+            
+            if (successText.equals("Logged In Successfully")) {
+                System.out.println("Login Success ✅");
+          
+                // langsung logout setelah login berhasil
+                WebElement logoutButton = driver.findElement(By.linkText("Log out"));
+                
+                logoutButton.click();
+                System.out.println("Logout After login : Success");
+            } else {
+                System.out.println("Login gagal ❌");
+            }
+            
+            
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            driver.quit(); // tutup browser
+        	// tutup browser
+            driver.quit(); 
         }
     }
 }
